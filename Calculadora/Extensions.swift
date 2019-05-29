95f394261834015b69c66ba64ff5f2c752117e3b
Luis Alberto Zagonel Pozenato <luis.pozenato@cwi.com.br>
2019-05-29 08:39:32 -0300

    Easey Blink

--- Calculadora/Extensions.swift
+++ Calculadora/Extensions.swift
@@ -15,8 +15,8 @@ extension UILabel {
     func startBlink() {
         UIView.animate(withDuration: 0.1,
                        delay:0.0,
-                       options:[.allowUserInteraction, .curveEaseInOut],
-                       animations: { self.alpha = 0 },
+                       options:[.allowUserInteraction, .curveEaseOut],
+                       animations: { self.alpha = 0.5 },
                        completion: { (_) -> Void in self.stopBlink() } )
     }
     
